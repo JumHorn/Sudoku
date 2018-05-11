e6134b406ef28ae90dedcaec61cbafb6b20f7200
JumHorn <zheyangkaishi@gmail.com>
2018-05-12 00:44:46 +0800

    finish file read

--- sudoku.cpp
+++ sudoku.cpp
@@ -1,4 +1,7 @@
 #include "sudoku.h"
+#include<iostream>
+#include<fstream>
+using namespace std;
 
 
 
@@ -11,11 +14,53 @@ Sudoku::~Sudoku()
 {
 }
 
+//read from a file which stores the numbers in it
+//9x9 and the unknows correspond to 0
 bool Sudoku::init()
-{}
+{
+	ifstream fin("sudoku.txt");
+	char c;
+	for(int i=0;i<9;i++)
+	{
+		for(int j=0;j<9;j++)
+		{
+			fin>>c;
+			grid[i][j].rightnum = c - '0';
+		}
+	}
+	return true;
+}
 
+//the algorithm to calculate the right number
 int Sudoku::calculate()
-{}
+{
+	return 0;
+}
 
+//save the result to file
 bool Sudoku::output()
-{}
+{
+	ofstream fout("result.txt");
+	for(int i=0;i<9;i++)
+	{
+		for(int j=0;j<9;j++)
+		{
+			fout<<grid[i][j].rightnum<<" ";
+		}
+		fout<<endl;
+	}
+	return true;
+}
+
+//print the result to the screen
+void Sudoku::print()
+{
+	for(int i=0;i<9;i++)
+	{
+		for(int j=0;j<9;j++)
+		{
+			cout<<grid[i][j].rightnum<<" ";
+		}
+		cout<<endl;
+	}
+}
--- sudoku.h
+++ sudoku.h
@@ -2,11 +2,12 @@
 #define _SUDOKU_H_
 
 //if the number not sure, filled with 0
+//if the rightnum is 0,it means not sure what exactly it is
 struct Grid
 {
-	int rightnum;
-	int possiblenum[9];
-	Grid() :rightnum(0)
+	int rightnum;		  //the correct number
+	int possiblenum[9];   //the possible number
+	Grid() : rightnum(0)
 	{
 		for (int i = 0; i < 9; i++)
 		{
@@ -25,6 +26,7 @@ public:
 	bool init();
 	int calculate();
 	bool output();
+	void print();
 };
 
 #endif
